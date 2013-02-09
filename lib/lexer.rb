19616b2710c364492cd89b8d1e72979b20971f07
Smit Shah <who828@gmail.com>
2013-02-09 14:27:25 +0530

    Added numbers and strings to the lexer

--- lib/lexer.rb
+++ lib/lexer.rb
@@ -8,16 +8,22 @@ class Lexer
     while i < clean_code.size
       chunk = clean_code[i..-1]
 
-      if identifier = chunk[/^([a-z]\w*)/]
+      if identifier = chunk[/^([a-z]\w*)/, 1]
         if KEYWORDS.include?(identifier)
           tokens << [identifier.upcase.to_sym, identifier]
         else
           tokens << [:IDENTIFIER, identifier]
         end
         i += identifier.size
-      elsif constant = chunk[/^([A-Z]\w*)/]
+      elsif constant = chunk[/^([A-Z]\w*)/, 1]
         tokens << [:CONSTANT, constant]
         i += constant.size
+      elsif number = chunk[/^(\d+)/, 1]
+        tokens << [:NUMBER, number.to_i]
+        i += number.size
+      elsif string = chunk[/^"(.*?)"/, 1]
+        tokens << [:STRING, string]
+        i += string.size + 2
       end
       i += 1
     end
--- spec/lexer_spec.rb
+++ spec/lexer_spec.rb
@@ -8,7 +8,6 @@ describe Lexer do
         if false:
           pass
     CODE
-
     Lexer.tokenize(code).should include([:IF, "if"], [:IDENTIFIER, "print"], [:IF, "if"])
     end
 
@@ -17,7 +16,20 @@ describe Lexer do
       Keywords = ['a', 'b', 'c']
       Pi = 3.14
     CODE
-
     Lexer.tokenize(code).should include([:CONSTANT, "Keywords"], [:CONSTANT, "Pi"])
+  end
+
+  it "tokenizes numbers in given code" do
+    code = <<-CODE
+      Keywords = [1234, 456, 'c']
+    CODE
+    Lexer.tokenize(code).should include([:NUMBER, 1234], [:NUMBER, 456])
+  end
+
+  it "tokenizes numbers in given code" do
+    code = <<-CODE
+      Keywords = ["abcde","ddecc"]
+    CODE
+    Lexer.tokenize(code).should include([:STRING, "abcde"], [:STRING, "ddecc"])
   end
 end
